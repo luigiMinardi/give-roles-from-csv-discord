c07739093ee594d61fc9401f1d40b711b48eb439
Luigi Minardi <lugminsinlag@gmail.com>
2023-04-13 22:47:15 +0200

    feat: Creating command to receive the kickstart data needed as csv.

--- commands/receiveKSDataAsCsv.js
+++ commands/receiveKSDataAsCsv.js
@@ -5,17 +5,17 @@ const { finished } = require('stream/promises');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('roles-csv')
-        .setDescription('Receive csv with role names and ids')
+        .setName('kickstarter-data')
+        .setDescription('Receive csv with discord tags and the respective role')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
         .setDMPermission(false)
         .addAttachmentOption(file => file
-            .setName('rolescsv')
-            .setDescription('Add a .csv file with role names and ids')
+            .setName('kscsv')
+            .setDescription('Add a .csv file with discord tags and role names')
             .setRequired(true)),
     async execute(interaction) {
-        const stream = fs.createWriteStream('roles.csv');
-        const { body } = await fetch(`${interaction.options.getAttachment('rolescsv').url}`);
+        const stream = fs.createWriteStream('kickstart.csv');
+        const { body } = await fetch(`${interaction.options.getAttachment('kscsv').url}`);
         await finished(Readable.fromWeb(body).pipe(stream));
         interaction.reply(`File received successfully.`); 
     },
